c38f41c3dba8a23cb4ef27326f508bca46b73eb3
Vinícius H <91447833+GaaraSan01@users.noreply.github.com>
2023-03-01 17:05:46 -0300

    adicionando funcionalidade ao form

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,23 +1,62 @@
 import { FormStyle, DivForm } from "./style"
 
+//Components React
+import { useState, useEffect } from "react"
+import emailjs from "@emailjs/browser"
+
 
 export const Form = () => {
-    return(
-        <FormStyle>
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [message, setMessage] = useState()
+
+    const sendEmail = (event) => {
+        event.preventDefault()
+        const templatParams = {
+            to_name: name,
+            e_mail: email,
+            message: message
+        }
+
+        emailjs.send("service_xgx8onc", "template_0v9dncg", templatParams, "uGY3ymnMzJ9pHQilO")
+        .then((response) => {
+            console.log("Enviado com sucesso!", response.status, response.text)
+        })
+        .catch((error) => console.error(error))
+    }
+
+    return (
+        <FormStyle onSubmit={sendEmail}>
             <DivForm>
                 <h1>Entrar em Contato</h1>
             </DivForm>
             <DivForm>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" id="name" />
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    onChange={(props) => setName(props.target.value)}
+                />
             </DivForm>
             <DivForm>
                 <label htmlFor="e-mail">E-mail:</label>
-                <input type="text" name="e-mail" id="e-mail" />
+                <input
+                    type="text"
+                    name="e-mail"
+                    id="e-mail"
+                    onChange={(props) => setEmail(props.target.value)}
+                />
             </DivForm>
             <DivForm>
                 <label htmlFor="phrase">Assunto:</label>
-                <textarea name="Assunto" id="phrase" cols="30" rows="2"></textarea>
+                <textarea
+                    name="Assunto"
+                    id="phrase"
+                    cols="30"
+                    rows="2"
+                    onChange={(props) => setMessage(props.target.value)}
+                ></textarea>
             </DivForm>
             <DivForm>
                 <button>Enviar</button>
